867209124892a360a16f57a9e353ada47b04f2e0
Chuch3x <c.augusto_mendez@hotmail.com>
2024-10-13 18:21:02 -0400

    added unit test for load_audios from assets.py file

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -5,31 +5,47 @@ from unittest import mock
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from assets import load_sprites, get_sprite, sprites
+from assets import load_sprites, get_sprite, load_audios, sprites, audios
 
-# Fixture para simular pygame.image.load
+
 @pytest.fixture
 def mock_pygame():
     with mock.patch("pygame.image.load") as mock_load:
-        mock_load.side_effect = lambda x: f"MockedImage({x})"  # Simula la carga de imágenes
+        mock_load.side_effect = lambda x: f"MockedImage({x})"
         yield mock_load
 
-# Fixture para simular os.listdir
+@pytest.fixture
+def mock_pygame_sound():
+    with mock.patch("pygame.mixer.Sound") as mock_sound:
+        mock_sound.side_effect = lambda x: mock.MagicMock()
+        yield mock_sound
+
 @pytest.fixture
 def mock_os_listdir():
     with mock.patch("os.listdir") as mock_listdir:
-        # Simula que hay tres imágenes en el directorio
         mock_listdir.return_value = ["0.png", "1.png", "2.png"]
         yield mock_listdir
         
 def test_load_sprites(mock_pygame, mock_os_listdir):
     load_sprites()
-    assert "sprite1" in sprites
-    assert "sprite2" in sprites
-    assert sprites["sprite1"] == "MockedImage(assets/sprites/sprite1.png)"
-    assert sprites["sprite2"] == "MockedImage(assets/sprites/sprite2.png)"
-
+    assert "0" in sprites
+    assert "1" in sprites
+    assert sprites["0"] == "MockedImage(assets/sprites/0.png)"
+    assert sprites["1"] == "MockedImage(assets/sprites/1.png)"
 
 def test_get_sprite(mock_pygame, mock_os_listdir):
     load_sprites()
     assert get_sprite("0") == "MockedImage(assets/sprites/0.png)"
+
+def test_get_sprite_not_found():
+    sprites.clear()
+    with pytest.raises(KeyError):
+        get_sprite("non_existent_sprite")
+
+def test_load_audios(mock_pygame_sound, mock_os_listdir):
+    mock_os_listdir.return_value = ["audio1.wav", "audio2.mp3"]
+    load_audios()
+    assert "audio1" in audios
+    assert "audio2" in audios
+    assert isinstance(audios["audio1"], mock.MagicMock)
+    assert isinstance(audios["audio2"], mock.MagicMock)
