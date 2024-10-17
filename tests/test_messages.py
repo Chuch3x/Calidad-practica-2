c9c60515c0a579341f445bbca3d8ad93f564806c
FranAliss <franalissar@gmail.com>
2024-10-17 09:34:18 -0400

    Game messages tested ssuccessfully

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,2 +1,14 @@
 from objects.gameover_message import *
-from objects.gameover_message import *
+from objects.gamestart_message import *
+import assets
+from layer import Layer
+
+def test_gameover_init():
+    game_over = GameOverMessage()
+    assert game_over._layer is Layer.UI
+    assert game_over.image is not None
+
+def test_gamestart_init():
+    game_start = GameStartMessage()
+    assert game_start._layer is Layer.UI
+    assert game_start.image is not None
